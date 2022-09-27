6f9a5a10b382b6ed66ccd2a52b623d65dccac1cd
Thibault Gajdos <thibault.gajdos@gmail.com>
2022-09-27 22:18:34 +0200

    repeat until can read RDS file

--- rw/fit_util.r
+++ rw/fit_util.r
@@ -1,5 +1,5 @@
 
-fit_indiv <- function (data, subject, model, delta = .99, iter = 10000, warmup = 5000){
+fit_indiv <- function (data, subject, model, delta = .99, iter = 20000, warmup = 10000){
     rstan_options(auto_write = FALSE)
     d <- data %>%
         filter(subjID == subject)
@@ -10,17 +10,26 @@ fit_indiv <- function (data, subject, model, delta = .99, iter = 10000, warmup =
         loss = abs(d$loss)
     )
     modelFile <- paste('./stan/',model,'.stan', sep = '')
-    fit <- stan(modelFile,
-                data = data_list,
-                iter = iter,
-                warmup = warmup,
-                chains = 4,
-                cores = 4,
-                init =  'random',
-                seed = 12345,
-                control = list(adapt_delta = delta,  max_treedepth = 12)
-                )
-    file_name = paste('./results/',model,'_',subject,'.rds', sep = '')
-    saveRDS(fit, file = file_name)
+    boolFalse <- F
+    while(boolFalse==F){
+        tryCatch({
+            fit <- stan(modelFile,
+                        data = data_list,
+                        iter = iter,
+                        warmup = warmup,
+                        chains = 4,
+                        cores = 4,
+                        init =  'random',
+                        seed = 12345,
+                        control = list(adapt_delta = delta,  max_treedepth = 12)
+                        )
+            file_name = paste('./results/',model,'_',subject,'.rds', sep = '')
+            saveRDS(fit, file = file_name)
+            x <- readRDS(file = file_name)
+            boolFalse <- T
+        },
+        error=function(e){},
+        finally={})
+    }
 }
 
